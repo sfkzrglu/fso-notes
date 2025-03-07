c6e34ddc416eac7f901a4f9a8a44f3787836f61b
Sefik ZIROGLU <sefikziroglu@outlook.com>
2025-03-07 18:04:28 +0100

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -25,10 +25,6 @@ let notes = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
