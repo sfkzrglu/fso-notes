5ba101ce1e50e1ff87881a52e4b50a3c3d8af53e
Sefik ZIROGLU <sefikziroglu@outlook.com>
2025-05-31 16:33:51 +0200

    errorHandler

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -36,10 +36,7 @@ app.get('/api/notes/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({error:'malformatted id'})
-        })
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -61,6 +58,19 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
